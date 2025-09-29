c6f4da92ec7a1f9e9e16cf8816981772303e7f4b
Halil İbrahim Aksoy <halilibrahimaksoy54@gmail.com>
2025-09-29 10:29:09 +0300

    Add transaction domain models and repository interface
    Introduces TransactionType enum, Transaction entity, and ITransactionRepository interface to define transaction data and repository operations for the finance tracker domain.

---
+++ FinanceTracker.Domain/Consts/TransactionType.cs
@@ -0,0 +1,8 @@
+﻿namespace FinanceTracker.Domain.Consts
+{
+    public enum TransactionType
+    {
+        Income =1,
+        Expense = 2
+    }
+}
---
+++ FinanceTracker.Domain/Entities/Transaction.cs
@@ -0,0 +1,13 @@
+﻿using FinanceTracker.Domain.Consts;
+
+namespace FinanceTracker.Domain.Entities
+{
+    public class Transaction
+    {
+        public Guid Id { get; set; }
+        public TransactionType Type { get; set; }
+        public decimal Amount { get; set; }
+        public string Category { get; set; } = string.Empty;
+        public DateTime Date { get; set; } = DateTime.UtcNow;
+    }
+}
